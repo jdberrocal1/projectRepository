da650306ca9a856a27c49109070595221e837d32
Daniel Berrocal <jdanielb1992@gmail.com>
2017-09-26 09:29:07 -0600

    importing project

--- src/components/projects/importProject.vue
+++ src/components/projects/importProject.vue
@@ -1,11 +1,59 @@
 <template>
   <div class="container-fluid">
-    <h3>Import Project</h3>
+    <h4 class="justifyText">To import projects from Jira please type in the ticket number!</h4>
+    <div class="row searchContainer">
+        <div class="col-xs-12">
+          <div id="custom-search-input">
+            <div class="input-group col-md-12">
+              <input type="text" class="form-control input-lg searchInput" placeholder="Ticket Number" v-model="ticketNumer" />
+              <span class="input-group-btn">
+                <button class="btn btn-lg" type="button" @click="importProject">
+                  Import
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      ticketNumer:'SANDBOXS-837'
+    }
+  },
+  methods:{
+    importProject(){
+      if(this.ticketNumer.length){
+        // this.$Progress.start();
+        // const requestConfig = {
+        //   headers: {
+        //     Authorization: 'Basic amJlcnJvY2FsLWFzOmRhbmllbDIwMTA=='
+        //     // 'Accept': 'application/json',
+        //     // 'Content-Type': 'application/json',
+        //     // 'Access-Control-Allow-Origin':'*',
+        //     // 'Access-Control-Allow-Headers': 'Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin'
+        //   }
+        // };
+        // this.$http.get('https://jira.avantica.net:7443/rest/api/2/issue/' + this.ticketNumer + '/editmeta')
+        //   .then(response => {
+        //     let data = response.body;
+        //     if (data && data.successful) {
+        //       console.log(data);
+        //     } else {
+        //     }
+        //     this.$Progress.finish();
+        //   })
+        //   .catch(error => {
+        //     console.log(error);
+        //     this.$Progress.fail();
+        //   });
+      }
+    }
+  }
 }
 </script>
 
