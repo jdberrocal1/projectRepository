1a62a0195922ea7dfa4bbe26d9d8058d3a564956
Daniel Berrocal <jdanielb1992@gmail.com>
2018-04-19 12:05:29 -0600

    adding project details

--- src/components/projects/projectDetail.vue
+++ src/components/projects/projectDetail.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="container-fluid">
     <h3>{{project.title}}</h3>
-    <p>{{project.description}}</p>
-    <p><b>Requirements: </b></p>
-    <p>{{project.requirements}}</p>
+    <div class="row">
+      <div class="col-xs-6 col-sm-6 col-md-3">
+        <p><b>Main Technology: </b> {{project.mainTechnology}}</p>
+      </div>
+      <div class="col-xs-6 col-sm-6 col-md-3">
+        <p><b>English Level: </b> {{project.englishLevel}}</p>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <p><b>Min Position: </b> {{project.minPosition}}</p>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3" v-if="project.maxPosition">
+        <p><b>Max Position: </b> {{project.maxPosition}}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <p><b>Task Description: </b></p>
+        <p class="description">{{project.taskDescription}}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <p><b>Requirements: </b></p>
+        <p>{{project.requirements}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +38,15 @@ export default {
   data() {
     return {
       project: []
+    }
+  },
+  computed:{
+    startDate() {
+      if (this.project.expectedStartDate) {
+        let date = new Date(this.project.expectedStartDate);
+        return `${date.getDate()}/${date.getMoth()}/${date.getFullYear()}`;
+      }
+      return '';
     }
   },
   created(){
@@ -35,6 +67,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.description {
+  text-align: justify;
+}
 </style>
